859c6bfbf8b64b2f94c8b6b98d1cf2ad566f6654
Santiago Rodríguez <88201812+santydesignscr@users.noreply.github.com>
2024-07-12 13:36:00 -0600

    Actualizar email.php

--- app/config/email.php
+++ app/config/email.php
@@ -4,5 +4,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['protocol'] = 'smtp';
 $config['mailtype'] = 'html';
 $config['wordwrap'] = TRUE;
-$config['wrapchars'] = 1000;
+$config['wrapchars'] = 998;
 $config['smtp_timeout'] = 300;
